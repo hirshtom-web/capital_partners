9efb0943d8110eb6f169b1dfadf5b583198cdf0c
hirshtom-web <hirshtom@gmail.com>
2025-11-06 12:06:46 +0200

    Refactor loadHTML to use fetch with error handling
    Updated loadHTML function to use fetch for loading HTML content and added error handling.

--- include.js
+++ include.js
@@ -1,3 +1,9 @@
-function loadHTML(selector, file, callback) { ... }
-loadHTML("#header", "header.html", () => { ... });
-loadHTML("#footer", "footer.html", () => { ... });
+function loadHTML(selector, file, callback) {
+  fetch(file).then(r => r.text()).then(data => {
+    document.querySelector(selector).innerHTML = data;
+    if (callback) callback();
+  }).catch(err => console.error('Error loading', file, err));
+}
+
+loadHTML("#header", "header.html", () => { feather.replace(); /* reattach menu & dropdowns */ });
+loadHTML("#footer", "footer.html", () => { feather.replace(); });
