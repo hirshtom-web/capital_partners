e742ae35270519ef0782b252363c33e4c10a6bcd
hirshtom-web <hirshtom@gmail.com>
2025-11-09 22:23:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -73,39 +73,3 @@ document.querySelectorAll('nav a[href="#"]').forEach(link => {
       document.getElementById(panelId).classList.add('active');
     });
   });
-
-
-function toggleMenu(el) {
-  const nav = document.getElementById('nav-menu');
-  nav.classList.toggle('active');
-  el.classList.toggle('active');
-}
-</script>
-<script src="https://d3js.org/d3.v7.min.js"></script>
-<script src="https://unpkg.com/topojson@3"></script>
-<script>
-d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json").then(topojsonData => {
-  const states = topojson.feature(topojsonData, topojsonData.objects.states);
-  const width = 960, height = 600;
-  const svg = d3.select("#map").append("svg")
-                .attr("viewBox", [0, 0, width, height])
-                .style("width", "100%")
-                .style("height", "auto");
-  const projection = d3.geoAlbersUsa().translate([width / 2, height / 2]).scale(1000);
-  const path = d3.geoPath().projection(projection);
-  const tooltip = d3.select("#tooltip");
-  svg.selectAll("path.state")
-     .data(states.features)
-     .enter()
-     .append("path")
-     .attr("class", "state")
-     .attr("d", path)
-     .on("mousemove", function (event, d) {
-        tooltip.style("opacity", 1)
-               .style("left", (event.pageX + 10) + "px")
-               .style("top", (event.pageY - 20) + "px")
-               .text(d.properties.name || "State");
-     })
-     .on("mouseout", () => tooltip.style("opacity", 0));
-});
-
